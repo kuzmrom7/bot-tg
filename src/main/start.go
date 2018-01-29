b20bf9e3ed0ea1758414c1d754c3882bab819e11
Kuzmrom7 <kuzmrom7@gmail.com>
2018-01-29 10:38:02 +0300

    test

--- src/main/start.go
+++ src/main/start.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
 func (telegramBot *TelegramBot) Start() {
 	for update := range telegramBot.Updates {
 		if update.Message != nil {
 			telegramBot.analyzeUpdate(update)
+		} else {
+			fmt.Println("GOOOOOOOOOOOO")
 		}
 	}
 }
