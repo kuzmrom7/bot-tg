4a400cc0ebc569fe8b8afc24fe0c1ebcac3f6676
Kuzmrom7 <kuzmrom7@gmail.com>
2018-01-29 10:51:44 +0300

    update inline

--- src/main/start.go
+++ src/main/start.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import "gopkg.in/telegram-bot-api.v4"
 
 func (telegramBot *TelegramBot) Start() {
 	for update := range telegramBot.Updates {
 		if update.Message != nil {
 			telegramBot.analyzeUpdate(update)
-		} else {
-			fmt.Println("GOOOOOOOOOOOO")
+		}
+		if update.CallbackQuery != nil {
+			class := update.CallbackQuery.Data
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, class+" Okey")
+			telegramBot.API.Send(msg)
+		}
 		}
 	}
 }
