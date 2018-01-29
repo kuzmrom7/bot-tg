f86356964dddf09dac30186cd5c58064436fef20
Kuzmrom7 <kuzmrom7@gmail.com>
2018-01-29 09:44:13 +0300

    fix

--- src/main/main.go
+++ src/main/main.go
@@ -45,30 +45,32 @@ func main() {
 		var msg tgbotapi.MessageConfig
 		log.Println("recived text: ", update.Message.Text)
 
+		if update.Message != nil {
+			switch update.Message.Text {
 
-		switch update.Message.Text {
+			case "/start":
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Привет, откуда поедешь? "+train)
 
-		case "/start":
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Привет, откуда поедешь? "+train)
+				keyboard := tgbotapi.InlineKeyboardMarkup{}
+				for _, class := range classes {
+					var row []tgbotapi.InlineKeyboardButton
+					btn := tgbotapi.NewInlineKeyboardButtonData(class, class)
+					row = append(row, btn)
+					keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, row)
+				}
 
-			keyboard := tgbotapi.InlineKeyboardMarkup{}
-			for _, class := range classes {
-				var row []tgbotapi.InlineKeyboardButton
-				btn := tgbotapi.NewInlineKeyboardButtonData(class, class)
-				row = append(row, btn)
-				keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, row)
+				msg.ReplyMarkup = keyboard
+
+			default:
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			}
-
-			msg.ReplyMarkup = keyboard
-
-		default:
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		}
+		} else  {
 
 		if update.CallbackQuery != nil {
-			class := update.CallbackQuery.Data
-			bot.Send(tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID,
-				"Ok, I remember"+class))
+		class := update.CallbackQuery.Data
+		bot.Send(tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID,
+		"Ok, I remember"+class))
+		}
 		}
 
 		bot.Send(msg)
